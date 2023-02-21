f9fdde4bfb9f2e1b31d0d0af2a0393bc02c79d2e
sasha.gwak <sasha.gwak@gmail.com>
2023-02-21 11:59:10 +0900

    ✨ [widget Lifecycle] initState, dispose 이해하기

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTile() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,8 +36,14 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
+            children: [
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                onPressed: toggleTile,
+                icon: const Icon(
+                  Icons.remove_red_eye,
+                ),
+              )
             ],
           ),
         ),
@@ -40,13 +54,37 @@ class _AppState extends State<App> {
 
 // 부모요소인 _AppState에서 theme 을 받아올건데, 이렇게 부모 요소에 접근하는 경우 context사용
 // BuildContext context는 자식 이전의 있는 모든 상위 요소들에 대한 정보를 가지고 있음
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  @override
+  // initState는 상태를 초기화하기위한 메서드
+  // 자주 사용되는 건 아니지만 종종 부모요소에 의존하는 데이터를 초기화해야하는 경우 쓰임
+  // initState는 항상 build보다 먼저 호출되어야함
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  // dispose는 위젯이 스크린에서 제거될 때 호출되는 메서드
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return Text(
       'My Large Title',
       style: TextStyle(
