cd91a736fe3be88d0b1c76fe6d9a23a8d7c0cf7b
sasha.gwak <sasha.gwak@gmail.com>
2023-02-21 11:43:13 +0900

    ✨ [stateful] buildContext 이해하기

--- lib/main.dart
+++ lib/main.dart
@@ -12,43 +12,48 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
-
-  void onClicked() {
-    // setState는 state 클래스에게 데이터가 변경되었다고 일리는 함수
-    // setState가 실행되면 build가 재실행됨
-    setState(() => {counter = counter + 1});
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // app 스타일 시트 지정
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Click Count',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              Text(
-                "$counter",
-                style: const TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              IconButton(
-                iconSize: 40,
-                onPressed: onClicked,
-                icon: const Icon(Icons.add_box_rounded),
-              ),
+            children: const [
+              MyLargeTitle(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+// 부모요소인 _AppState에서 theme 을 받아올건데, 이렇게 부모 요소에 접근하는 경우 context사용
+// BuildContext context는 자식 이전의 있는 모든 상위 요소들에 대한 정보를 가지고 있음
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        // 위젯 트리로부터 theme을 가져오게함
+        // !은 확실히 있는 경우, ?은 있는 경우 가져오도록 함
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
