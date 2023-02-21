4c7bc966b4f392c9fdb420b7e2c4c01c27a582c8
sasha.gwak <sasha.gwak@gmail.com>
2023-02-21 11:24:12 +0900

    ✨ [Stateful] setState 이해하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,155 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widget/button.dart';
-import 'package:toonflix/widget/currency_card.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    // setState는 state 클래스에게 데이터가 변경되었다고 일리는 함수
+    // setState가 실행되면 build가 재실행됨
+    setState(() => {counter = counter + 1});
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // 지정색깔 OxFF 적고 쓰거나, Color.fromARGB 사용
-        backgroundColor: const Color(0xFF181818),
-        // column은 수직, row는 수평 배열
-        // row의 MainAxisAlignment는 수평방향
-        // column의 MainAxisAlignment는 수직방향
-        // SingleChildScrollView 스크롤링 할 수 있도록 해줌
-        body: SingleChildScrollView(
-          child: Padding(
-            // all은 상하좌우에 모두 padding 10, only로 상화좌우중 하나 선택
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              // 간격을 주기위해서는 SizedBox 사용
-              children: [
-                const SizedBox(
-                  height: 70,
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(
+                  fontSize: 30,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                            color: Colors.amber,
-                            fontSize: 38,
-                            fontWeight: FontWeight.w800,
-                          ),
-                        ),
-                        Text(
-                          'Welcome back',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.8),
-                            fontSize: 18,
-                          ),
-                        )
-                      ],
-                    )
-                  ],
+              ),
+              Text(
+                "$counter",
+                style: const TextStyle(
+                  fontSize: 30,
                 ),
-                const SizedBox(
-                  height: 60,
-                ),
-                Text(
-                  'Total balance',
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white.withOpacity(0.8),
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Text(
-                  '\$5 194 482',
-                  style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    // container는 child를 가지고 있는 단순한 box, html의 div같은거
-                    Button(
-                      text: 'Transfer',
-                      bgColor: Colors.amber,
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: 'Request',
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text(
-                      'Wallets',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.8),
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CurrencyCard(
-                  name: 'Euro',
-                  code: 'EUR',
-                  amount: '6 428',
-                  icon: Icons.euro_rounded,
-                  isInverted: false,
-                  order: 0,
-                ),
-                const CurrencyCard(
-                  name: 'Bitcoin',
-                  code: 'BTC',
-                  amount: '9 785',
-                  icon: Icons.currency_bitcoin_rounded,
-                  isInverted: true,
-                  order: 1,
-                ),
-                const CurrencyCard(
-                  name: 'Dollar',
-                  code: 'USD',
-                  amount: '428',
-                  icon: Icons.attach_money_outlined,
-                  isInverted: false,
-                  order: 2,
-                ),
-              ],
-            ),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+              ),
+            ],
           ),
         ),
       ),
