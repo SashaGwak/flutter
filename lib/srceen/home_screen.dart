901472e834fb49d8c3bf233fc993606eff3f8a44
sasha.gwak <sasha.gwak@gmail.com>
2023-02-22 14:04:21 +0900

    ⚡️ [webtoon] FutureBuilder + ListView 사용해보기

--- lib/srceen/home_screen.dart
+++ lib/srceen/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:toonflix/service/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class HomeScreen extends StatelessWidget {
           backgroundColor: Colors.white,
           foregroundColor: Colors.green,
           title: const Text(
-            "오늘의 웹툰",
+            "샤툰",
             style: TextStyle(
               fontSize: 24,
             ),
@@ -29,10 +29,34 @@ class HomeScreen extends StatelessWidget {
           builder: (context, snapshot) {
             // future가 완료되어 데이터가 존재하는 지 확인
             if (snapshot.hasData) {
-              return const Text('There is data!');
+              return MakeList(snapshot);
             }
-            return const Text('Loading...');
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           },
         ));
+  }
+
+  ListView MakeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      // user가 보여주는 화면의 데이터만 데꼬옴! WOW
+      scrollDirection: Axis.horizontal, // 가로방향
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Text(webtoon.title);
+      },
+      // ListView.separated 쓰면 separatorBuilder(공백만들어주기) 사용가능
+      separatorBuilder: (context, index) => const SizedBox(width: 20),
+    );
+
+    // 많은 양의 데이터를 연속적으로 보여주려면 ListView쓰는게 좋음
+    // 그래도 ListView를 써서 한번에 모든 데이터를 가져오는 것은 효율적이지 않음 -> ListView.builder사용(보이는 데이터만 불러오기)
+    // return ListView(
+    //   children: [
+    //     for (var webtoon in snapshot.data!) Text(webtoon.title)
+    //   ],
+    // );
   }
 }
