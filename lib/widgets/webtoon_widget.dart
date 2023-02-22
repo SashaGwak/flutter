226420437d290865720e052b7d3eb17c2114dbd0
sasha.gwak <sasha.gwak@gmail.com>
2023-02-22 17:52:44 +0900

    ✨ [webtoon] Detail screen 연결하기(Navigator.push)

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/srceen/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final String title, thumb, id;
@@ -13,7 +14,23 @@ class Webtoon extends StatelessWidget {
   Widget build(BuildContext context) {
     // GestureDetector는 대부분의 동작을 감지하도록 해줌
     return GestureDetector(
-      onTap: () {},
+      // onTap은 클릭했을때 이벤트 실행
+      onTap: () {
+        // Navigator.push를 사용하면 애니매이션 효과를 이용해 다른페이지에 온것처럼 보이게 할 수 있음
+        // Navigator.push(context, route);
+        // MaterialPageRoute는 StatelessWidget를 감싸서 다른 route처럼 보이게 만들어줌
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              title: title,
+              thumb: thumb,
+              id: id,
+            ),
+            // fullscreenDialog: true, // 밑에서부터 새로운 페이지올라오도록
+          ),
+        );
+      },
       child: Column(
         children: [
           Container(
